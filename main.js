e4ebb39297faab6e9f995c7fc6c638f7896e154a
ddeabeqirii <167891907+ddeabeqirii@users.noreply.github.com>
2024-10-03 16:20:58 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+console.log("digital school")
+
+ 
+
+var a = 50 + 50
+var b = 19 - 12
+var c = 23 - 45
+
+
+console.log(a)
+console.log(b)
+console.log(c)
+
+a+=15
+console.log(a)
+
+var number1 = "20"
+var number2 = 20
+
+console.log(numri1 == number2)
